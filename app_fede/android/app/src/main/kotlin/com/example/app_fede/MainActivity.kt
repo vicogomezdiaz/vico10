935e7f6ec7c3d7844ffd5a538767f888da4a61ad
vico <65690540+vicogomezdiaz@users.noreply.github.com>
2020-09-05 13:09:16 -0400

    Add files via upload

---
+++ app_fede/android/app/src/main/kotlin/com/example/app_fede/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_fede
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_fede/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}      material app 
+         home: Scaraffold
+          appbar : appbar(
+            title:Text(app navajo cosina del alma)
+              bagroundcolor: color blak
+              
